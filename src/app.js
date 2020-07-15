afd100776b1e38daa08b4fd82fe78f3bd6b9856a
Christian Petrov <cpetrov@eclipsesource.com>
2020-07-15 20:28:58 +0200

    Improve variable naming
    Change-Id: I407e28ee4123e0cbcafecd05eda1b3eac120c86a

--- src/app.js
+++ src/app.js
@@ -4,9 +4,9 @@ const JSZip = require('jszip');
   const response = await fetch('https://tabris.com/wp-content/uploads/2020/07/zipfile.zip')
   const archive = await response.arrayBuffer();
   const result = await JSZip.loadAsync(archive);
-  result.filter((path, file) => !file.dir)
-  .forEach(async file => {
-    const contents = await file.async('string');
-    console.log('Path:\n', file.name, '\nContents:\n', contents, '\n')
-  });
+  result.filter((path, entry) => !entry.dir)
+    .forEach(async file => {
+      const contents = await file.async('string');
+      console.log('Path:\n', file.name, '\nContents:\n', contents, '\n')
+    });
 })().catch(error => console.error(error));
